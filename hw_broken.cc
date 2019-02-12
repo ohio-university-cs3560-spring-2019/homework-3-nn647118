4daba07edd201a3068d6de7935a08be2ac3aba7e
Nathan Nelson <nnelson@pu1.cs.ohiou.edu>
2019-02-11 19:54:23 -0500

    added doxygen and latex files

--- hw_broken.cc
+++ hw_broken.cc
@@ -4,21 +4,29 @@
 
 
 
-int deviation( int* a, int n )
+/// deviation - calculates the standard devition of a given array
+///
+/// @param a - the integer array
+/// @param n - the size of the integer array
+/// @return double standard deviation
+double deviation(int* a, int n)
 {
-	int sum;
-	for(size_t i = 0; i <= v.size(); i++)
+	int sum = 0;
+	std::vector<int> v;
+	v.assign(a, a + n);
+	for (size_t i = 0; i < v.size(); i++)
 	{
 		sum += v[i];
-	} 
-	double mean = sum /= v.size();
+	}
+	double mean = double(sum) / double(v.size());
 	double stddev = 0;
-	for(size_t i = 0; i <= v.size() -1; i++)
+	for (size_t i = 0; i <= v.size() - 1; i++)
 	{
-		double stddev = stddev + (v[i] - mean) * (v[i] - mean); 
+		stddev = stddev + (v[i] - mean) * (v[i] - mean);
 	}
 	stddev /= v.size();
-	if( stddev = 0)
+	if (stddev == 0)
 		std::cout << "Sigma is zero." << std::endl;
+
 	return sqrt(stddev);
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-int deviation( int* a, int n);
+double deviation( int* a, int n);
 
 int main()
 {
 	  int a[4] = {1,2,3,4};
-		std::cout << deviation(a) << std::endl;
+	  std::cout << deviation(a, 4) << std::endl;
 }
